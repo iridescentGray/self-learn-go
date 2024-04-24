5e8b77eb6fa7d2ba773156bb6ff0431e8905f2fc
colorfulgray0 <colorfulgray0@gmail.com>
2024-04-24 17:26:41 +0800

    feat: learn func

---
+++ go_base/function/function_value.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func Double(n int) int {
+	return n + n
+}
+
+func Apply(n int, f func(int) int) int {
+	return f(n)
+}
+
+func main() {
+	fmt.Printf("%T\n", Double) // func(int) int
+	// 默认值为nil
+	var f func(n int) int = Double
+	g := Apply
+	fmt.Printf("%T\n", g) // func(int, func(int) int) int
+
+	fmt.Println(f(9))         // 18
+	fmt.Println(g(6, Double)) // 12
+	fmt.Println(Apply(6, f))  // 12
+}
