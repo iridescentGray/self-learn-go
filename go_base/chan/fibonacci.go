f7a977f6fe4ce15dbd79add76f51af01f7720310
colorfulgray0 <colorfulgray0@gmail.com>
2024-04-28 18:15:13 +0800

    feat: learn go

---
+++ go_base/chan/fibonacci.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fibonacci := func() chan uint64 {
+		c := make(chan uint64)
+		go func() {
+			var x, y uint64 = 0, 1
+			for ; y < (1 << 63); c <- y { // 步尾语句
+				x, y = y, x+y
+			}
+			close(c)
+		}()
+		return c
+	}
+	c := fibonacci()
+	for x, ok := <-c; ok; x, ok = <-c { // 初始化和步尾语句
+		time.Sleep(time.Second)
+		fmt.Println(x)
+	}
+}
